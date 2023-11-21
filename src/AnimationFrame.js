c34a4c537416c93d05ccb547043a0c0b1d7f72a8
tynan Sampsel <tynansampselr@gmail.com>
2023-11-21 10:09:09 -0400

    added drawing, clearing, and saving.

---
+++ src/AnimationFrame.js
@@ -0,0 +1,9 @@
+import React from "react";
+function AnimationFrame(props) {
+
+  return (
+    <img className="AnimationFrame"></img>
+  );
+}
+
+export default AnimationFrame;
--- src/App.js
+++ src/App.js
@@ -1,9 +1,44 @@
+import React from "react";
+import {useRef, useState} from "react";
+import ReactDOM from "react-dom";
+import CanvasDraw from "react-canvas-draw";
+
 import './App.css';
 
 function App() {
+  
+  const canvasDrawRef = useRef(null);
+
+  const [image, setImage] = useState("")
+
+  const canvasDrawClear = () => {
+    if (!canvasDrawRef.current) return
+
+    canvasDrawRef.current.clear();
+  }
+
+  const canvasDrawExport = () => {
+    if (!canvasDrawRef.current) return
+    
+    const img = canvasDrawRef.current.getDataURL("img", false, "#FFF");
+    setImage(img);
+    console.log(img);
+  }
+
+
+  const canvasDrawProps = {
+    backgroundColor: "#FFF",
+    brushRadius: 2,
+    brushColor: "#ff0000",
+  };
+
   return (
     <div className="App">
-      
+      <CanvasDraw ref={canvasDrawRef} {...canvasDrawProps}/>
+      <button onClick={canvasDrawClear}>Clear!</button>
+      <button onClick={canvasDrawExport}>Export</button>
+
+      <img src={image} alt="Embedded Image" />
     </div>
   );
 }
